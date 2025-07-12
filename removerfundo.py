24ec4bdfd959d24b6acf81b9a72ad97655aed017
Marcela Nako <123596770+maabenako@users.noreply.github.com>
2025-07-12 09:21:31 -0300

    script que remove fundo das fotos

--- removerfundo.py
+++ removerfundo.py
@@ -1,9 +1,9 @@
-from rembg import remove
-from PIL import Image
+from rembg import remove  # Importo a função mágica que remove o fundo da imagem
+from PIL import Image     # Biblioteca para manipular imagens no Python (amo o poder do Pillow 💅)
 
-input_path = 'cl.jpg'
-output_path = 'output2.png'
+input_path = 'cl.jpg'     # Caminho da imagem original (com fundo)
+output_path = 'output2.png'  # Nome da imagem que vai sair sem fundo
 
-input = Image.open(input_path)
-output = remove(input)
-output.save(output_path)
+input = Image.open(input_path)  # Abro a imagem que quero editar
+output = remove(input)          # Removo o fundo com uma única função (muito chique)
+output.save(output_path)        # Salvo a nova imagem com fundo transparente
